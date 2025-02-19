062d9405b2cb5db220d22fec719835a535b62107
AliHassanSiddiqui24 <alihassansiddiqui24@gmail.com>
2025-02-19 22:54:09 +0500

    Lab 4 Updated

---
+++ Week 2/Lab 2/Self Task 2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        calculator calc = new calculator();
+            int x, y;
+            int choice = mainmenu();
+            while (true)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the first number");
+                        x=int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the second number");
+                        y=int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Addition: x+y= {calc.add(x,y)}");
+                        break;
+                        case 2:
+                        Console.WriteLine("Enter the first number");
+                        x = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the second number");
+                        y = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"subtraction: x-y = {calc.subtract(x,y)}");
+                        break;
+                        case 3:
+                        Console.WriteLine("Enter the first number");
+                        x = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the second number");
+                        y = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"multiplication: x*y= {calc.multiply(x,y)}");
+                        break;
+                        case 4:
+                        Console.WriteLine("Enter the first number");
+                        x = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the second number");
+                        y = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"division: x/y = {calc.divide(x,y)}"); 
+                        break;
+                }
+                choice = mainmenu();
+            }
+        }
+        public static int mainmenu()
+        {
+            Console.WriteLine("basic calculator");
+
+            Console.WriteLine("ADDITION");
+            Console.WriteLine("subtraction");
+            Console.WriteLine(("multiplication"));
+            Console.WriteLine("division");
+            int option;
+            option=int.Parse(Console.ReadLine());
+            return option;
+        }
+    }
+}
---
+++ Week 2/Practice/Example 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red Color text");
+            Console.ForegroundColor= ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green Color Background");
+            Console.BackgroundColor= ConsoleColor.Black;
+            Console.WriteLine("Default Color");
+        }
+    }
+}
